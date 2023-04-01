fc05c06365f5bfb977aa0d8558e05984d6d58e68
Ahmed-Elkady12 <ahmed.abdalkader@aktrading.io>
2023-04-01 22:59:23 +0200

    fix: add cost for bookings table

--- database/car_hire_system.sql
+++ database/car_hire_system.sql
@@ -20,7 +20,7 @@ CREATE TABLE vehicle_types (
 CREATE TABLE vehicles (
   vehicle_id INT PRIMARY KEY auto_increment,
   type_id INT NOT NULL,
-  available BOOLEAN,
+  available BOOLEAN ,
   FOREIGN KEY (type_id) REFERENCES vehicle_types(type_id)
 );
 
@@ -31,6 +31,7 @@ CREATE TABLE bookings (
   vehicle_id INT,
   hire_date DATE,
   return_date DATE,
+  cost FLOAT NOT NULL,
   FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
   FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id)
 );
